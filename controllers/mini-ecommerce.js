846f1ce1903e28112bc51f89bb51baa9df12aa99
thiagoalgo <thiagoalgo@gmail.com>
2021-03-01 22:02:40 -0300

    criados servicos para mini-ecommerce

---
+++ controllers/mini-ecommerce.js
@@ -0,0 +1,23 @@
+import cidadesEstados from '../cidades-estados.js'
+
+function finalizarCompra(req, res) {
+  console.log(req.body)
+  res.send('ok')
+}
+
+function obterCidadesPorEstado(req, res) {
+  const siglaEstado = req.params['siglaEstado'].toUpperCase()
+  console.log(siglaEstado)
+  const dadosEstado = cidadesEstados.estados.filter(estado => estado.sigla === siglaEstado)
+  console.log(dadosEstado)
+  if (dadosEstado.length === 0) {
+    res.status(404).json({ erro: `${siglaEstado} não um estado válido` })
+  } else {
+    res.json(dadosEstado[0].cidades)
+  }
+}
+
+export {
+  finalizarCompra,
+  obterCidadesPorEstado
+}
--- index.js
+++ index.js
@@ -5,10 +5,14 @@ import {
   listarTarefaId,
   listarTarefas,
   cadastrarTarefa,
-  atualizarTarefa, 
+  atualizarTarefa,
   excluirTarefa,
   concluirTarefa
 } from './controllers/gerenciador-tarefa.js'
+import {
+  finalizarCompra,
+  obterCidadesPorEstado
+} from './controllers/mini-ecommerce.js'
 
 const app = express()
 const port = 3001
@@ -16,13 +20,16 @@ const port = 3001
 app.use(cors())
 app.use(bodyParser.json())
 
-const path = '/gerenciador-tarefas'
+const pathGerTar = '/gerenciador-tarefas'
+app.get(pathGerTar, listarTarefas)
+app.get(`${pathGerTar}/:id`, listarTarefaId)
+app.post(pathGerTar, cadastrarTarefa)
+app.put(`${pathGerTar}/:id`, atualizarTarefa)
+app.delete(`${pathGerTar}/:id`, excluirTarefa)
+app.put(`${pathGerTar}/:id/concluir`, concluirTarefa)
 
-app.get(path, listarTarefas)
-app.get(`${path}/:id`, listarTarefaId)
-app.post(path, cadastrarTarefa)
-app.put(`${path}/:id`, atualizarTarefa)
-app.delete(`${path}/:id`, excluirTarefa)
-app.put(`${path}/:id/concluir`, concluirTarefa)
+const pathMiniEcomm = '/mini-ecommerce'
+app.get(`${pathMiniEcomm}/estado/:siglaEstado/cidades`, obterCidadesPorEstado)
+app.post(`${pathMiniEcomm}/checkout/finalizar-compra`, finalizarCompra)
 
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`))
